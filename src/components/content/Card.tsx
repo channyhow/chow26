10be33d93308f8ce7814b41f0972919e363f1ab7
Channy <117459099+channyhow@users.noreply.github.com>
2026-09-12 13:00:26 +0200

    Center project card Mux media

--- src/components/content/Card.tsx
+++ src/components/content/Card.tsx
@@ -33,7 +33,10 @@ export function Card({
   const mediaRef = Array.isArray(item.media) ? item.media[0] : item.media;
   const media = resolveMedia(mediaRef);
   const isProject = Boolean(item.href?.startsWith("/projets/"));
-  const mediaOrientation = getMediaOrientation(media ?? undefined);
+  const cardMedia = isProject && media?.type === "mux"
+    ? { ...media, focalPoint: { x: 50, y: 50 } }
+    : media;
+  const mediaOrientation = getMediaOrientation(cardMedia ?? undefined);
   const projectMission = isProject ? getProjectMission(item) : undefined;
   const visibleItem = isProject
     ? {
@@ -52,13 +55,13 @@ export function Card({
 
   const content = (
     <>
-      {media ? (
+      {cardMedia ? (
         <div
           className="card__mediaWrap"
-          data-media-type={media.type}
+          data-media-type={cardMedia.type}
           data-orientation={mediaOrientation}
         >
-          <Media media={media} className="card__media" />
+          <Media media={cardMedia} className="card__media" />
         </div>
       ) : null}
       <TextBlock content={visibleItem} titleAs="h3" className="card__body" />
